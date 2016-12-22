e9ddb49f3adc80aae65ee9a7670156c31fe92319
jtenner <tenner.joshua@gmail.com>
2016-12-22 10:24:57 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+
+let { sync } = require('glob');
+let { basename, extname } = require('path');
+let { writeFile } = require('fs');
+
+module.exports = class PluginBuilder {
+  constuctor(opts) {
+    this.entry = opts.entry;
+    this.folder = resolve(opts.folder);
+  }
+  apply(compiler) {
+    let files = sync(join(this.folder, '*.js'));
+    let source = `module.exports = {${files.map((file) => {
+      let name = basename(file, extname(file));
+      return `$'{name}': require('${file}')`;
+    }).join(',')}};`;
+
+    compiler.plugin('compilation', (ctx, cb) => writeFile(compiler.options.entry[this.entry], source, cb));
+  }
+};
