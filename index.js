ac11f0483a2ed513eb36a2c9dbc440ca60651885
jtenner <tenner.joshua@gmail.com>
2016-12-22 10:40:41 -0500

    fix small bugs

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 let { sync } = require('glob');
-let { basename, extname } = require('path');
+let { basename, extname, join, resolve } = require('path');
 let { writeFile } = require('fs');
 
-module.exports = class PluginBuilder {
-  constuctor(opts) {
+module.exports = class LibrarySourcePlugin {
+  constructor(opts) {
     this.entry = opts.entry;
+
     this.folder = resolve(opts.folder);
   }
   apply(compiler) {
+    console.log(compiler.options);
     let files = sync(join(this.folder, '*.js'));
     let source = `module.exports = {${files.map((file) => {
       let name = basename(file, extname(file));
-      return `$'{name}': require('${file}')`;
+      return `'${name}': require('${file}')`;
     }).join(',')}};`;
 
     compiler.plugin('compilation', (ctx, cb) => writeFile(compiler.options.entry[this.entry], source, cb));
